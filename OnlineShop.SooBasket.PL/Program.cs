46f3dac9a405947d5f1d8ad1f06edb34d99a20f2
denzi <denziksar@gmail.com>
2023-03-05 21:38:09 +0400

    add new project for pl

---
+++ OnlineShop.SooBasket.PL/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.SooBasket.EFDal.DbContexts;
+using OnlineShop.SooBasket.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var config = builder.Configuration.GetSection("EnvironmentVariables").Get<EnvironmentVariables>();
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<NpgsqlContext>(
+    options =>
+    {
+        options.UseNpgsql(config?.NpgsqlConnectionString);
+    });
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    //app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+//app.UseRouting();
+
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller}/{action=Index}/{id?}");
+
+//app.MapFallbackToFile("index.html");
+var _context = app.Services.GetService<NpgsqlContext>();
+var list= _context?.Categories.ToList();
